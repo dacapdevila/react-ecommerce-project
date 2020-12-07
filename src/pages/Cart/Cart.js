9f3a780880114775a0bd5362f94332645d40c632
Dario L Capdevila <dariocapdevila@gmail.com>
2020-12-07 12:51:49 -0300

    Eliminacion de funciones en desuso

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
 import CartContext from "../../globals/cartContext";
 import CartTable from "../../components/CartTable/CartTable";
 import Summary from "../../components/Summary/Summary";
@@ -7,22 +6,6 @@ import CartMessage from "../../components/CartMessage/CartMessage";
 
 const Cart = () => {
     const { cart } = useContext(CartContext);
-
-    const messageConditional = (
-        <div className="card text-center cart-message">
-            <div className="card-header">
-                El carrito esta vacio
-            </div>
-            <div className="card-body">
-                <p className="card-text">
-                    Agrega productos y luego podrás finalizar la compra
-                </p>
-                <Link to="/" className="btn btn-primary">
-                    Ir al home para continuar comprando
-                </Link>
-            </div>
-        </div>
-    );
 
     return (
         <div className="cart">
