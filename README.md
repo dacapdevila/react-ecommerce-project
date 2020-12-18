97af562cf42d6b73f8a3c0899717367e7a3dfb9d
Dario L Capdevila <dariocapdevila@gmail.com>
2020-12-17 22:08:40 -0300

    Actualizacion en readme, eliminacion de imagenes de react, agregado de imagenes de la categoria alfajores

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Se abrirá una ventana de navegación http://localhost:3000.
 ![image5](https://raw.githubusercontent.com/dacapdevila/react-ecommerce-project/master/src/assets/react_carrito_sin_productos.png)
 
 ## Demostración del circuito de compra
-![image4](https://raw.githubusercontent.com/dacapdevila/react-ecommerce-project/master/src/assets/circuito_compra.gif)
+![image6](https://raw.githubusercontent.com/dacapdevila/react-ecommerce-project/master/src/assets/circuito_compra.gif)
 
 ## Ver proyecto en vivo
 [Ver demo](https://react-ecommerce-project-deu49.ondigitalocean.app/).
--- src/media/images.js
+++ src/media/images.js
@@ -3,11 +3,17 @@ import Fanta from "../assets/fanta.webp";
 import SevenUp from "../assets/seven-up.webp";
 import Manaos from "../assets/manaos.webp";
 import PepsiLight from "../assets/pepsi-light.webp";
+import GuaymallenDulceDeLeche from "../assets/guaymallen-dulce-de-leche.webp";
+import TripleMilkaDulceDeLeche from "../assets/triple-milka-dulce-de-leche.webp";
+import VauquitaNegro from "../assets/vauquita-negro.webp";
 
 export const Images = {
     CocaCola,
     Fanta,
     SevenUp,
     Manaos,
-    PepsiLight
+    PepsiLight,
+    GuaymallenDulceDeLeche,
+    TripleMilkaDulceDeLeche,
+    VauquitaNegro
 };
